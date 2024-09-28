b463727e16349e62551506ae140bdb88269a1289
pattylo <19451945as@gmail.com>
2024-09-29 01:49:35 +0800

    update a bit

--- bootcamp_scripts/4_walker_control/g_control_partitioning/Lec17c_control_partitioning/3a_pendulum_pd/control.m
+++
@@ -1,9 +0,0 @@
-function T1 = control(t,x,parms)
-
-theta1 = x(1);                          
-theta1dot = x(2);  
-
-%the controller
-%SIMPLE PD CONTROL
-%tau = -kp*(q - qd) - kd*qdot
-T1 = -parms.control.Kp1*(theta1-parms.control.theta1ref)-parms.control.Kd1*theta1dot;
