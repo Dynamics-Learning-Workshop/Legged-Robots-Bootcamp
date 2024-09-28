68c07188e490b6abf7e2f1a1604b53067c7dd470
pattylo <19451945as@gmail.com>
2024-09-29 01:48:36 +0800

    fix angle problem tmr

---
+++ bootcamp_scripts/4_walker_control/g_control_partitioning/single_pendulum_control_partitioning.py
@@ -0,0 +1,88 @@
+import sys
+import os
+import numpy as np
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+from dynamics import Integrator as inte, RobotUtils as util
+
+g = 9.81
+l1 = 1.0
+m1 = 1.0
+I1 = 0.02
+
+
+t = 0
+sample_factor = 10
+
+# simulation environment
+q0_all_rk4 = []
+
+t_step = 1e-3
+t_all = []
+
+q0=np.pi + 0.2
+u0=-0.0
+x_rk4 = np.array([q0, u0])
+
+q0_ref = 0
+Kp = 2
+Kd = 1
+
+def draw_anime(success):
+    if success:
+        print('SYSTEM INTEGRATION SUCCEEDED...')
+        save_name = "single_pendulum"
+    else:
+        print('SYSTEM INTEGRATION FAILED...')
+        save_name = "single_pendulum" + "_failed"
+    
+    inte().anime(
+        t=t_all[::sample_factor], 
+        x_states=[
+            q0_all_rk4[::sample_factor]
+        ], 
+        ms=1000 * t_step * sample_factor,
+        mission="Swing", 
+        sim_object="single_pendulum",
+        sim_info={'l1': l1},
+        save=False,
+        save_name=save_name
+    )
+    exit()
+
+def f_single_pendulum(x):
+    
+    theta0 = x[0]
+    omega0 = x[1]
+    
+    
+    
+    tau = -Kp * (theta0 - q0_ref) - Kd * omega0
+    print(theta0 - q0_ref)
+    
+    A = 1.0*I1 + 0.25*l1**2*m1
+    b = -g*l1*m1*np.sin(theta0)/2 + tau
+    
+    alpha0 = -b/A
+    
+    return np.array([
+        omega0, 
+        alpha0
+        ])
+    
+t_lim = 2.0
+
+while True:
+    x_rk4_new = inte().rk4(f_single_pendulum, x=x_rk4, h=t_step)
+            
+    q0_all_rk4.append(x_rk4_new[0])
+    
+    t = t + t_step
+    t_all.append(t)
+
+    x_rk4 = x_rk4_new
+    
+    if t > t_lim:
+        break
+    
+draw_anime(True)
