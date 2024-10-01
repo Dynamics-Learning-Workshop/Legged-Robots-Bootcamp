64f4844301260878900997eb9fdeca77d4fcc225
pattylo <19451945as@gmail.com>
2024-10-02 01:31:11 +0800

    wewerw

--- bootcamp_scripts/4_walker_control/g_control_partitioning/a!_Lec17c_control_partitioning/6_pendulum_pid/control.m
+++
@@ -1,8 +0,0 @@
-function T1 = control(t,x,parms)
-
-theta1 = x(1);                          
-theta1dot = x(2); 
-theta1sum = x(3);
-
-%the controller
-T1 = -parms.control.Kp1*(theta1-parms.control.theta1ref)-parms.control.Kd1*theta1dot-parms.control.Ki1*theta1sum;
