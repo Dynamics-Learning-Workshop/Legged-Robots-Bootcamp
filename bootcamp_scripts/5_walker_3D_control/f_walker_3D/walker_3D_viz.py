e554cf7f33ac367fe64aff792b2560217c9029a9
pattylo <19451945as@gmail.com>
2024-10-20 22:54:44 +0800

    update!

--- bootcamp_scripts/5_walker_3D_control/f_walker_3D/walker_3D_viz.py
+++ bootcamp_scripts/5_walker_3D_control/f_walker_3D/walker_3D_viz.py
@@ -31,41 +31,42 @@ param = np.array([w, l0, l1, l2])
 
 x,y,z,roll,pitch,yaw, roll_lh,pitch_lh,yaw_lh, roll_rh,pitch_rh,yaw_rh, pitch_lk, pitch_rk = q
 
-p_H = get_p_H(*q,*param) # head
+
+p_H = sim3D().get_p_H(*q,*param) # head
 print(p_H)
-p_B = get_p_B(*q,*param)
+p_B = sim3D().get_p_B(*q,*param)
 print(p_B)
 
-p_LH = get_p_LH(*q,*param)
+p_LH = sim3D().get_p_LH(*q,*param)
 print(p_LH)
-p_RH = get_p_RH(*q,*param)
+p_RH = sim3D().get_p_RH(*q,*param)
 print(p_RH)
 
-p_LK = get_p_LK(*q,*param)
+p_LK = sim3D().get_p_LK(*q,*param)
 print(p_LK)
-p_RK = get_p_RK(*q,*param)
+p_RK = sim3D().get_p_RK(*q,*param)
 print(p_RK)
 
-p_LA = get_p_LA(*q,*param)
+p_LA = sim3D().get_p_LA(*q,*param)
 print(p_LA)
-p_RA = get_p_RA(*q,*param)
+p_RA = sim3D().get_p_RA(*q,*param)
 print(p_RA)
 
-p_Torso = get_p_Torso(*q,*param)
+p_Torso = sim3D().get_p_Torso(*q,*param)
 print(p_Torso)
 
-p_Thigh_L = get_p_Thigh_L(*q,*param)
+p_Thigh_L = sim3D().get_p_Thigh_L(*q,*param)
 print(p_Thigh_L)
-p_Thigh_R = get_p_Thigh_R(*q,*param)
+p_Thigh_R = sim3D().get_p_Thigh_R(*q,*param)
 print(p_Thigh_R)
 
-p_Calf_L = get_p_Calf_L(*q,*param)
+p_Calf_L = sim3D().get_p_Calf_L(*q,*param)
 print(p_Calf_L)
-p_Calf_R = get_p_Calf_R(*q,*param)
+p_Calf_R = sim3D().get_p_Calf_R(*q,*param)
 print(p_Calf_R)
 
 print("==========")
-p_Hip_I = get_p_Hip_L_init(0,0,0,*q[3:],*param)
+p_Hip_I = sim3D().get_p_Hip_L_init(0,0,0,*q[3:],*param)
 print(p_Hip_I)
 
 q[0:3] = p_Hip_I
